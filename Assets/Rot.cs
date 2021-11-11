8956b2927f630292940f096992c5230e69a27436
sapphire-al2o3 <t_telkun@hotmail.com>
2021-11-11 20:24:12 +0900

    立方体を回転させる

---
+++ Assets/Rot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rot : MonoBehaviour
+{
+    void Update()
+    {
+		transform.Rotate(0, Time.deltaTime * 100.0f, 0);
+    }
+}
